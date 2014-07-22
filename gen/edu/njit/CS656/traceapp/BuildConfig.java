6fddc3ebdec2baf610f1b3779fb6fd672091f77d
bentonjimmy <benton.jimmy@gmail.com>
2014-07-22 09:28:30 -0400

    Initial Push
    The code needs to be cleaned up but this should be everything in the
    project.  Icons still need to be added and the app needs to be tested.

---
+++ gen/edu/njit/CS656/traceapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.njit.CS656.traceapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/njit/CS656/traceapp/MainActivity.java
@@ -0,0 +1,223 @@
+package edu.njit.CS656.traceapp;
+
+import java.util.List;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity 
+implements GooglePlayServicesClient.ConnectionCallbacks,
+			com.google.android.gms.location.LocationListener,
+			GooglePlayServicesClient.OnConnectionFailedListener{
+	
+	private GoogleMap theMap;
+	private LocationClient myLocationClient;
+	private PolylineOptions lineOptions;
+	private Polyline line;
+	private boolean tracking = false;
+	
+	private final static LocationRequest REQUEST = LocationRequest.create()
+			.setInterval(2000).setFastestInterval(500).setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+	/*
+	 * This is called when the app is first started.  The initial view is set and the Map is retrieved.
+	 */
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+	   super.onCreate(savedInstanceState);
+	   setContentView(R.layout.activity_main);
+	   getMap();
+   }
+   
+   /*
+    * This method runs when the app is active.  The map is retrieved along with the location client.
+    */
+   @Override
+   protected void onResume()
+   {
+	   super.onResume();
+	   getMap();
+	   getLocationClient();
+	   initializeCamera();
+   }
+   
+   /*
+    * This needs to continue to receive new location updates but they don't need to be refreshed on
+    * the map.
+    */
+   @Override
+   protected void onPause()
+   {
+	   super.onPause();
+   }
+   
+   @Override
+   protected void onStop()
+   {
+	   //This should close all connections 
+	   super.onStop();
+	   if(myLocationClient != null)
+	   {
+		   myLocationClient.disconnect();
+	   }
+   }
+   
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu)
+   {
+	   //Inflate the menu; this adds items to the action bar
+	   //This will need Start, Stop, Pause, Reset
+	   MenuInflater inflater = getMenuInflater();
+	   inflater.inflate(R.menu.main, menu);
+	   return super.onCreateOptionsMenu(menu);
+   }
+   
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item)
+   {
+	   //Handles actions in the action bar
+	   switch(item.getItemId())
+	   {
+	   		case R.id.action_settings:
+			   //do something
+			   return true;
+	   		case R.id.startTracking:
+	   			startTracking();
+	   			return true;
+	   		case R.id.stopTracking:
+	   			stopTracking();
+	   			return true;
+	   		default:
+	   			return super.onOptionsItemSelected(item);
+	   }
+   }
+   
+   protected void getMap()
+   {
+	   if(theMap == null)
+	   {
+		   theMap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+		   if(theMap != null)
+		   {
+			   theMap.setMyLocationEnabled(true); //allow my location to be found
+			   
+		   }
+	   }
+   }
+   
+   protected void getLocationClient()
+   {
+	   if(myLocationClient == null)
+	   {
+		   myLocationClient = new LocationClient(getApplicationContext(), this, this);
+		   if(myLocationClient != null)
+		   {
+			   myLocationClient.connect();
+		   }
+		   
+	   }
+   }
+
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		updateLocation(location.getLatitude(), location.getLongitude());
+		
+	}
+	
+	@Override
+	public void onConnected(Bundle bundle) {
+		myLocationClient.requestLocationUpdates(REQUEST, this);
+		
+	}
+	
+	
+	private void updateLocation(double latitude, double longitude)
+	{
+		theMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+				new CameraPosition.Builder().target(new LatLng(latitude, longitude))
+				.zoom(15.5f).bearing(0).tilt(25).build()));
+		
+		//Only add the points if we are tracking the user
+		if(tracking)
+		{
+			addPoint(latitude, longitude);
+		}
+	}
+	
+	private void initializeCamera()
+	{
+		Location lastLoc = myLocationClient.getLastLocation();
+		theMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+				new CameraPosition.Builder().target(new LatLng(lastLoc.getLatitude(), lastLoc.getLongitude()))
+				.zoom(15.5f).bearing(0).tilt(25).build()));
+	}
+	
+	private void addPoint(double latitude, double longitude)
+	{
+		if(lineOptions == null)
+		{
+			lineOptions = new PolylineOptions().add(new LatLng(latitude, longitude))
+					.color(Color.BLUE).width(35).geodesic(false);
+			line = theMap.addPolyline(lineOptions);
+		}
+		if(line != null)
+		{
+			List<LatLng> points = line.getPoints();
+			points.add(new LatLng(latitude, longitude));
+			line.setPoints(points);
+		}
+	}
+   
+	/*
+	 * This method is used to start tracking the user's movements
+	 * and painting them to the map
+	 */
+	public void startTracking()
+	{
+		tracking = true;
+	}
+	
+	/*
+	 * This method is used to stop tracking the user and to stop painting the line
+	 * on the map
+	 */
+	public void stopTracking()
+	{
+		tracking = false;
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
